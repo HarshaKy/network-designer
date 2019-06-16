c6c9b9ca25b68e319fdf76b28b43b1ad85be84ef
HarshaKy <harshaky6573@gmail.com>
2019-06-16 14:02:40 +0530

    basic layout and improved folder structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 console.log('app running')
+
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+const port = 3000
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+	res.render('index', {
+		title: 'Network Diagram App',
+		name1: 'Harsha KY',
+		name2: 'Hari' 
+	})
+})
+
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
+})
